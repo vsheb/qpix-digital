5fe07917c05bb811d1268d7d3e019ec024d75a52
Kurtis Nishimura <kurtisn@SURFACE-KURTIS.localdomain>
2019-07-22 14:26:29 -1000

    Minor comment addition to make convention clear

--- firmware/src/QpixPkg.vhd
+++ firmware/src/QpixPkg.vhd
@@ -3,7 +3,12 @@ use IEEE.STD_LOGIC_1164.all;
 
 package QpixPkg is
 
-   -- Define some enums for making directional connections
+   -- Create a new type, with indexing scheme:
+   --    bitArray2d(row)(col)(dir)
+   --    dir decoding: 0 - right
+   --                  1 - down
+   --                  2 - left
+   --                  3 - up
    constant RT : integer := 0;
    constant DN : integer := 1;
    constant LT : integer := 2;
